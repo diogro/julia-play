83d46eb44722b711191134d42c250d94f0b5540e
diogro <diogro@gmail.com>
2012-07-10 16:53:46 -0300

    un-paralel

--- RS_Series.jl
+++ RS_Series.jl
@@ -21,39 +21,42 @@ end
 
 function Random_Skewers(A::Array{Float64, 2}, B::Array{Float64, 2})
     n, p = size(A)
-    n_vector = 1000
+    n_vector = 1000 
     betas = rand(n, n_vector)
-    deltaZ_A = distribute(A*betas, 2)
-    deltaZ_B = distribute(B*betas, 2)
-    RS = dzeros(n_vector)
-    @parallel for i=1:n_vector
+    deltaZ_A = A*betas
+    deltaZ_B = B*betas
+    RS = zeros(n_vector)
+    for i=1:n_vector
         RS[i] = (deltaZ_A[:,i]'*deltaZ_B[:,i]/
-                sqrt((deltaZ_A[:,i]'*deltaZ_A[:,i])*(deltaZ_B[:,i]'*deltaZ_B[:,i])))[1]
+                sqrt((deltaZ_A[:,i]'*deltaZ_A[:,i])'*(deltaZ_B[:,i]'*deltaZ_B[:,i])))[1]
     end
     mean(RS)
 end
 
 function RS_Series (Mats::Array{Float64, 3}, Target::Array{Float64, 2})
     n, p, NP = size(Mats)
-    Sim = dzeros(NP)
-    #    DMats = distribute(Mats)
-    DMats = Mats
-    #    @parallel for i=1:NP
+    Sim = zeros(NP)
     for i=1:NP
-        Sim[i] = Random_Skewers(DMats[:,:,i],Target)
+        Sim[i] = Random_Skewers(Mats[:,:,i],Target)
     end
     Sim
 end
 
-omega = dlmread("./G-target.csv","\t")
-mat_cor = zeros(10, 10, 10000, 20)
-mat_var = zeros(10, 10, 10000, 20)
-for i in 1:20
-    dir =
-    "/home/diogro/MainProject/Modularidade/MatrizB/Direcional/IntensidadeSelecao/IntSel$(i*10)/" 
-    println(dir)
-    cor_aux = (dlmread("$(dir)correlacao.txt"," "))[:,2:end] 
-    var_aux = (dlmread("$(dir)varP.txt", " "))[:,2:end]
-    mat_var[:,:,:,i], mat_cor[:,:,:,i] = CorrVarCreate(var_aux, cor_aux)
+function RS_Sel_Type (File::String) 
+    omega = dlmread("/home/diogro/julia-play/G-target.csv","\t")
+    mat_cor = zeros(10, 10, 10000, 20)
+    mat_var = zeros(10, 10, 10000, 20)
+    Results = zeros(10000, 20)
+    for i in 1:20
+        dir =
+        "/home/diogro/MainProject/Modularidade/MatrizB/Direcional/$(File)/IntSel$(i*10)/" 
+        println(dir)
+        cor_aux = (dlmread("$(dir)correlacao.txt"," "))[:,2:end] 
+        var_aux = (dlmread("$(dir)varP.txt", " "))[:,2:end]
+        mat_var[:,:,:,i], mat_cor[:,:,:,i] = CorrVarCreate(var_aux, cor_aux)
+        Results[:,i] = RS_Series(mat_var[:,:,:,i], omega)
+    end
+    dlmwrite("RS_Results_$(File)", Results, ' ')
 end
-Results = [RS_Series(mat_var[:,:,:,i], omega) for i in 1:20]
+RS_Sel_Type("MinForModule")
+#RS_Sel_Type("Corridor")
